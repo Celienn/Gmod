5d9d7c896d279c6ceb0d50ca9008947801b8fc77
Celienn <81368874+Celienn@users.noreply.github.com>
2022-05-31 04:05:20 +0200

    1

---
+++ html/background.js
@@ -0,0 +1,18 @@
+function setup(){
+    createCanvas(window.innerWidth,window.innerHeight)
+}
+
+function draw(){
+    background(50)
+    loadPixels();
+    for (var y = 0; y < window.innerHeight; y++){
+        for (var x = 0; x < window.innerWidth; x++){
+            var index = 0
+
+            
+        }
+    }
+    for (var test=0;test<50000;test++){
+        pixels[test] = 255;
+    }
+}
